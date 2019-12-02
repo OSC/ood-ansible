d8de8c56ce3e1b6b8d4261da1a45b13d783413c0
Jeff Ohrstrom <johrstrom@hotmail.com>
2019-12-02 09:23:41 -0500

    check_synatx shouldn't be looking for  anymore

--- tests/check_syntax.sh
+++ tests/check_syntax.sh
@@ -4,4 +4,4 @@ TESTDIR="$( cd "$(dirname "$0")" || . ; pwd -P )"
 # shellcheck source=./utils.sh
 . "$TESTDIR/utils.sh" 
 
-ansible-playbook -vvv -i "$INV_FILE" "$PLAYBOOK" --syntax-check || exit 1
+ansible-playbook -vvv "$PLAYBOOK" --syntax-check || exit 1
