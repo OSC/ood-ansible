48d09a33fd90c7b8e7db8a4c4eddb9579fd6469f
Guilherme Balzana <gb637@cam.ac.uk>
2020-01-17 12:28:59 +0000

    Remove trailing whitespaces
    * Add ansible_managed template

--- tests/check_syntax.sh
+++ tests/check_syntax.sh
@@ -2,6 +2,6 @@
 
 TESTDIR="$( cd "$(dirname "$0")" || . ; pwd -P )"
 # shellcheck source=./utils.sh
-. "$TESTDIR/utils.sh" 
+. "$TESTDIR/utils.sh"
 
 ansible-playbook -vvv "$PLAYBOOK" --syntax-check || exit 1
