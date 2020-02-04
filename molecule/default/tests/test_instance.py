8e650433f06f9efe9f10fcd1816c47747aec68b9
Guilherme Balzana <gb637@cam.ac.uk>
2020-02-04 15:49:41 +0000

    Fix rnode_uri config setting
    Fix rnode_uri option using node_uri as a value

--- molecule/default/tests/test_instance.py
+++ molecule/default/tests/test_instance.py
@@ -11,3 +11,8 @@ def test_ood_portal_conf(host):
     ood_portal_conf = '/opt/rh/httpd24/root/etc/httpd/conf.d/ood-portal.conf'
     header = '# Ansible managed:'
     assert host.file(ood_portal_conf).contains(header)
+
+
+def test_rnode_config(host):
+    ood_portal_yml = '/etc/ood/config/ood_portal.yml'
+    assert host.file(ood_portal_yml).contains('rnode_uri: /myrnode')
