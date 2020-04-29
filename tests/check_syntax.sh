cb4c1fe4e1788fde9b6291de2198028ed7dab2ac
Jeff Ohrstrom <johrstrom@hotmail.com>
2020-04-29 15:53:40 -0400

    move travis-ci tests to molecule

--- tests/check_syntax.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TESTDIR="$( cd "$(dirname "$0")" || . ; pwd -P )"
-# shellcheck source=./utils.sh
-. "$TESTDIR/utils.sh"
-
-ansible-playbook -vvv "$PLAYBOOK" --syntax-check || exit 1
--- tests/run_playbook.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-TESTDIR="$( cd "$(dirname "$0")" ; pwd -P )"
-. $TESTDIR/utils.sh
-
-ansible-playbook $PLAYBOOK $@ || exit 1
